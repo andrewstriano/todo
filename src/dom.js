bf05156c9c5a3087daf7c6aca161b3536bc9d070
andrewstriano <111657219+andrewstriano@users.noreply.github.com>
2023-04-06 19:50:26 -0700

    Create addTask functionality

--- src/dom.js
+++ src/dom.js
@@ -4,26 +4,42 @@ function expandProjectList() {
 }
 export const projectDropDownButton = document.querySelector(".expand");
 projectDropDownButton.addEventListener("click", expandProjectList);
-export const modal = document.querySelector(".projectModal");
+export const modalContainer = document.querySelector(".projectModal");
 export const formContainer = document.querySelector(".formContainer");
 export const content = document.querySelector(".content");
 
 function closeModal() {
-  modal.classList.remove("active");
-  modal.classList.add("inactive");
+  modalContainer.classList.remove("active");
+  modalContainer.classList.add("inactive");
   content.classList.remove("isBlurred");
 }
 
 function openModal() {
-  modal.classList.remove("inactive");
-  modal.classList.add("active");
+  modalContainer.classList.remove("inactive");
+  modalContainer.classList.add("active");
   content.classList.add("isBlurred");
 
-  modal.addEventListener("click", (e) => {
-    if (e.target !== formContainer) {
+  modalContainer.addEventListener("click", (e) => {
+    if (e.target === modalContainer) {
       closeModal();
     }
   });
 }
+export const addTaskButton = document.querySelector(".addTaskButton");
+function createTaskInput() {
+  const list = document.querySelector("#formList");
+  const li = document.createElement("li");
+  const label = document.createElement("label");
+  const input = document.createElement("input");
+  label.setAttribute("for", "task");
+  label.innerText = "New Task:";
+  input.setAttribute("id", "task");
+  input.setAttribute("type", "text");
+  li.append(label, input);
+  list.insertBefore(li, addTaskButton.parentNode);
+}
+
+addTaskButton.addEventListener("click", createTaskInput);
+
 export const addProjectButton = document.querySelector(".addProjectButton");
 addProjectButton.addEventListener("click", openModal);
