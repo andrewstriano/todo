fc3ca0b9a6219e3930f069a4467741541220ce63
andrewstriano <111657219+andrewstriano@users.noreply.github.com>
2023-04-08 18:46:00 -0700

    task changes

--- src/dom.js
+++ src/dom.js
@@ -28,17 +28,51 @@ function openModal() {
   });
 }
 export const addTaskButton = document.querySelector(".addTaskButton");
+let taskCount = 0;
 function createTaskInput() {
   const list = document.querySelector("#formList");
   const li = document.createElement("li");
-  const label = document.createElement("label");
-  const input = document.createElement("input");
-  label.setAttribute("for", "task");
-  label.innerText = "New Task:";
-  input.setAttribute("class", "task");
-  input.setAttribute("type", "text");
-  li.append(label, input);
+  const taskNameLabel = document.createElement("label");
+  const taskName = document.createElement("input");
+  taskNameLabel.setAttribute("for", "task");
+  taskNameLabel.innerText = "Task Name:";
+  taskName.setAttribute("class", "task");
+  taskName.setAttribute("type", "text");
+  taskName.dataset.taskCount = taskCount;
+
+  const taskDueDateLabel = document.createElement("label");
+  const taskDueDate = document.createElement("input");
+  taskDueDateLabel.setAttribute("for", "taskDueDate");
+  taskDueDateLabel.innerText = "Task Due:";
+  taskDueDate.setAttribute("class", "taskDueDate");
+  taskDueDate.setAttribute("type", "date");
+  taskDueDate.dataset.taskCount = taskCount;
+
+  const taskPriorityLabel = document.createElement("label");
+  const taskPriority = document.createElement("input");
+  taskPriorityLabel.setAttribute("for", "taskPriority");
+  taskPriorityLabel.innerText = "Task Priority:";
+  taskPriority.setAttribute("class", "taskPriority");
+  taskPriority.setAttribute("type", "number");
+  taskPriority.dataset.taskCount = taskCount;
+
+  taskNameLabel.setAttribute("for", "task");
+  taskNameLabel.innerText = "Task Name:";
+  taskName.setAttribute("class", "task");
+  taskName.setAttribute("type", "text");
+  taskName.dataset.taskCount = taskCount;
+
+  li.append(
+    taskNameLabel,
+    taskName,
+    taskDueDateLabel,
+    taskDueDate,
+    taskPriorityLabel,
+    taskPriority
+  );
   list.insertBefore(li, addTaskButton.parentNode);
+  taskCount++;
+  console.log(taskCount);
 }
 
 addTaskButton.addEventListener("click", createTaskInput);
@@ -48,41 +82,9 @@ addProjectButton.addEventListener("click", openModal);
 
 const projects = [];
 
-function createProject() {
-  let taskNodeList = document.querySelectorAll(".task");
-  let tasks = [];
-  for (let i = 0; i < taskNodeList.length; i++) {
-    const val = taskNodeList[i].value;
-    tasks.push(val);
-  }
-  let name = document.querySelector("#name").value;
-  let dueDate = document.querySelector("#dueDate").value;
-  let priority = document.querySelector("#priority").value;
-  let project = new Project(name, dueDate, tasks, priority);
-
-  projects.push(project);
-  createProjectSideBarButton(project);
-}
-
-function completeProject(event) {
-  let proj = event.target.project;
-  let index = projects.indexOf(proj);
-  projects.splice(index, 1);
-  event.target.parentNode.parentNode.remove();
-  console.log(projects);
-  refreshSideBarList();
-}
-
-function refreshSideBarList() {
-  document.querySelector(".projectContainer").innerHTML = "";
-  projects.forEach((project) => {
-    createProjectSideBarButton(project);
-  });
-}
-
 function completeTask(event) {
-  let index = event.target.index;
-  let project = event.target.project;
+  const index = event.target.index;
+  const project = event.target.project;
   event.target.project.tasks.splice(index, 1);
   event.target.parentNode.remove();
 }
@@ -96,7 +98,7 @@ function displayProject(event) {
   heading.classList.add("projectHeading");
 
   const title = document.createElement("h1");
-  title.innerText = project.name;
+  title.innerText = `Title: ${project.name}`;
 
   const dueDate = document.createElement("h2");
   dueDate.innerText = project.dueDate;
@@ -128,7 +130,6 @@ function displayProject(event) {
   document.querySelector(".main").append(displayContainer);
   console.log(project);
 }
-
 function createProjectSideBarButton(project) {
   const button = document.createElement("div");
   button.classList.add("project");
@@ -136,6 +137,36 @@ function createProjectSideBarButton(project) {
   button.innerText = project.name;
   button.addEventListener("click", displayProject);
   projectContainer.appendChild(button);
+}
+
+function createProject() {
+  const taskNodeList = document.querySelectorAll(".task");
+  const tasks = [];
+  for (let i = 0; i < taskNodeList.length; i++) {
+    const val = taskNodeList[i].value;
+    tasks.push(val);
+  }
+  const name = document.querySelector("#name").value;
+  const dueDate = document.querySelector("#dueDate").value;
+  const priority = document.querySelector("#priority").value;
+  const project = new Project(name, dueDate, tasks, priority);
+
+  projects.push(project);
+  createProjectSideBarButton(project);
+}
+
+function refreshSideBarList() {
+  document.querySelector(".projectContainer").innerHTML = "";
+  projects.forEach((project) => {
+    createProjectSideBarButton(project);
+  });
+}
+function completeProject(event) {
+  const proj = event.target.project;
+  const index = projects.indexOf(proj);
+  projects.splice(index, 1);
+  event.target.parentNode.parentNode.remove();
+  refreshSideBarList();
 }
 
 export const createProjectButton = document.querySelector(
